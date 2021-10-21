b5ba4fc40af99fbc0ac9852bb3ac0094e29d823d
juan <juanmondragon206@gmail.com>
2021-10-20 21:50:16 -0700

    no scrollbar

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Footer from '../components/section5/footer'
 
 export default function Home() {
   return (
-    <div className="flex flex-col  min-h-screen  ">
+    <div className="flex flex-col  min-h-screen overflow-x-hidden ">
       <Head>
         <title>Your Online Record Store</title>
         <link rel="icon" href="/favicon.ico" />
